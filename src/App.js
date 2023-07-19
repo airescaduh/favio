2cc943b3f456ae4f5addcd7b9ac5188e82ab678b
carloseduardoaires <aireseduardocarlos@gmail.com>
2023-07-19 10:38:41 -0300

    conteudo

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import Cabecalho from "./componentes/Cabecalho";
 import Rodape from "./componentes/Rodape";
-import Conteudo, { OlaMundo } from "./componentes/Conteudo";
+import Conteudo from "./componentes/Conteudo";
 /*
 Arquivo App JSX
 Componente funcional = Porque ele é uma função
 */
 
 function App() {
-  OlaMundo();
   /*Todo componente deve retornar um HTML, entre em parênteses*/
   return (
     /*O React substitui className por class pq class é palavra reservada do JS*/
     <>
       <Cabecalho logo="logo192.png" titulo="fav.io" subtitulo="O melhor site" />
-      <Rodape />
-      <Conteudo />
+      <Conteudo titulo="A"/>
+      <Rodape titulo="Rodapé" subtitulo="Caduh"/>
     </>
   );
 }
--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import styles from "../Cabecalho/Cabecalho.module.css";
 
-export default function Conteudo() {
-  return <div>Conteudo</div>;
-}
-export function OlaMundo() {
-  console.log("OlaMundo");
+export default function Conteudo(props) {
+  return (
+    <div className={styles.conteudo}>
+    <p className={styles.titulo}>{props.titulo}</p>
+    <p className={`${styles.titulo} ${styles.subtitulo}`}>{props.subtitulo}</p>
+  </div>
+  );
 }
