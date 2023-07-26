ac6f872620bd309513bfd0e10f4b1bb17fb01359
carloseduardoaires <aireseduardocarlos@gmail.com>
2023-07-26 11:24:41 -0300

    aaa

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Cabecalho.module.css";
 
 export default function Cabecalho(props) {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+const toggleMenu = () => {
+  console.log("Menu antes de alternar:", menuAberto);
+  setMenuAberto(!menuAberto);
+  console.log("Menu após alternar:", !menuAberto);
+};
+
   return (
     <div className={styles.cabecalho}>
-          <img src={props.logo} />
+      <img src={props.logo} alt="Logo" />
       <p className={styles.titulo}>{props.titulo}</p>
       <p className={`${styles.titulo} ${styles.subtitulo}`}>{props.subtitulo}</p>
+
+
+      {/* Menu Hambúrguer */}
+      <div className={`${styles.hamburguer} ${menuAberto ? styles.aberto : ""}`} onClick={toggleMenu}>
+        {/* Opções de Link */}
+        <a href="#home" className={styles.link} onClick={toggleMenu}>Home</a>
+        <a href="#produtos" className={styles.link} onClick={toggleMenu}>Produtos</a>
+        <a href="#sobre" className={styles.link} onClick={toggleMenu}>Sobre</a>
+      </div>
     </div>
   );
 }
