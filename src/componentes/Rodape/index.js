18287c31e48ee9dcb43b71e3189b26d967c29204
carloseduardoaires <aireseduardocarlos@gmail.com>
2023-09-15 08:23:03 -0300

    Revert "aaa"
    This reverts commit ac6f872620bd309513bfd0e10f4b1bb17fb01359.

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,31 +1,12 @@
 import React from "react";
 import styles from "../Cabecalho/Cabecalho.module.css";
-import tt from "../../twitter.png";
-import insta from "../../instagram.png";
-import wts from "../../whatsapp.png";
 
-const Rodape = () => {
-  const obterDataAtual = () => {
-    const data = new Date();
-    const dia = String(data.getDate()).padStart(2, '0');
-    const mes = String(data.getMonth() + 1).padStart(2, '0');
-    const ano = data.getFullYear();
-    return `${dia}/${mes}/${ano}`;
-  };
 
+export default function Rodape(props) {
   return (
     <div className={styles.rodape}>
-      <div className={styles.dataCorrente}>
-        Data atual: {obterDataAtual()}
-      </div>
-
-      <div className={styles.redes}>
-        <img className={styles.png} src={tt} alt="Twitter"></img>
-        <img className={styles.png} src={insta} alt="Instagram"></img>
-        <img className={styles.png} src={wts} alt="WhatsApp"></img>
-      </div>
-    </div>
+    <p className={styles.titulo}>{props.titulo}</p>
+    <p className={`${styles.titulo} ${styles.subtitulo}`}>{props.subtitulo}</p>
+  </div>
   );
-};
-
-export default Rodape;
+}
