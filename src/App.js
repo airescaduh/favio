23feaf82aadb5d42426dad82d247fdb6ed176fe7
carloseduardoaires <aireseduardocarlos@gmail.com>
2023-09-15 08:25:19 -0300

    temp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import "./App.css";
 import Cabecalho from "./componentes/Cabecalho";
 import Rodape from "./componentes/Rodape";
 import Conteudo from "./componentes/Conteudo";
-<<<<<<< HEAD
 import Menu from "./componentes/Menu";
 import Estados from "./componentes/Estados";
-=======
->>>>>>> parent of 7b72b39 (Atualização)
 /*
 Arquivo App JSX
 Componente funcional = Porque ele é uma função
@@ -18,17 +15,14 @@ function App() {
   /*O React substitui className por class pq class é palavra reservada do JS*/
   return (
     <>
-<<<<<<< HEAD
       {/* <Menu /> */}
       {/* <Cabecalho logo="logo192.png" titulo="fav.io" subtitulo="O melhor site" /> */}
       {/* <Conteudo titulo="A" /> */}
       <Estados />
       {/* <Rodape titulo="Rodapé" subtitulo="Caduh" /> */}
-=======
       <Cabecalho logo="logo192.png" titulo="fav.io" subtitulo="O melhor site" />
-      <Conteudo titulo="A"/>
-      <Rodape titulo="Rodapé" subtitulo="Caduh"/>
->>>>>>> parent of 7b72b39 (Atualização)
+      <Conteudo titulo="A" />
+      <Rodape titulo="Rodapé" subtitulo="Caduh" />
     </>
   );
 }
