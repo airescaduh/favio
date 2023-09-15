63b4d96da94f0172e1617b4aa6f2f2cf39dd7477
carloseduardoaires <aireseduardocarlos@gmail.com>
2023-09-15 08:22:44 -0300

    Revert "Atualização"
    This reverts commit 7b72b3978147a5cf519f7bebe4cd9456e236a5e9.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 import Cabecalho from "./componentes/Cabecalho";
 import Rodape from "./componentes/Rodape";
 import Conteudo from "./componentes/Conteudo";
+<<<<<<< HEAD
 import Menu from "./componentes/Menu";
 import Estados from "./componentes/Estados";
+=======
+>>>>>>> parent of 7b72b39 (Atualização)
 /*
 Arquivo App JSX
 Componente funcional = Porque ele é uma função
@@ -15,11 +18,17 @@ function App() {
   /*O React substitui className por class pq class é palavra reservada do JS*/
   return (
     <>
+<<<<<<< HEAD
       {/* <Menu /> */}
       {/* <Cabecalho logo="logo192.png" titulo="fav.io" subtitulo="O melhor site" /> */}
       {/* <Conteudo titulo="A" /> */}
       <Estados />
       {/* <Rodape titulo="Rodapé" subtitulo="Caduh" /> */}
+=======
+      <Cabecalho logo="logo192.png" titulo="fav.io" subtitulo="O melhor site" />
+      <Conteudo titulo="A"/>
+      <Rodape titulo="Rodapé" subtitulo="Caduh"/>
+>>>>>>> parent of 7b72b39 (Atualização)
     </>
   );
 }
